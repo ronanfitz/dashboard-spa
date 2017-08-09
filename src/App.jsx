d0ddf581d0eff8c6aee9fc57261a65d521de0e87
Michael Murray <michaelmurray6298@gmail.com>
2017-08-09 11:19:37 -0700

    Initial transit-widget integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,38 @@
 import React from 'react';
 import ReactGridLayout from 'react-grid-layout';
 import TransitComponent from '@databraid/transit-widget/lib';
+import {
+  TRANSIT_WIDGET_ID,
+  SLACK_WIDGET_ID,
+  GITHUB_WIDGET_ID,
+} from './constants';
 import './App.css';
 // import { Root as ExapleComponent } from  '@databraid/example-widget/lib';
+const Grid = ReactGridLayout.WidthProvider(ReactGridLayout);
 
 const App = () => {
   // layout is an array of objects, see the demo for more complete usage
   const layout = [
     {
       i: 'a',
-      x: 0,
+      x: 6,
       y: 0,
-      w: 5,
-      h: 2.4,
+      w: 6,
+      h: 10,
       minH: 2.4,
     },
-    { i: 'b', x: 0, y: 0, w: 12, h: 2.5 },
-    { i: 'c', x: 0, y: 0, w: 2.5, h: 2.5 },
+    { i: 'b', x: 3, y: 0, w: 6, h: 12 },
+    { i: 'c', x: 0, y: 0, w: 6, h: 10 },
   ];
   return (
-    <ReactGridLayout
-      className="layout"
-      layout={layout}
-      cols={12}
-      rowHeight={30}
-      width={1200}
-    >
+    <Grid className="layout" layout={layout} cols={12} rowHeight={30}>
       <div className="transit" key={'a'}>
-        <TransitComponent />
+        <TransitComponent widgetId={TRANSIT_WIDGET_ID} />
       </div>
 
-      <div key={'b'}>b</div>
-      <div key={'c'}>c</div>
-    </ReactGridLayout>
+      <div key={'b'}>GitHub</div>
+      <div key={'c'}>Slack</div>
+    </Grid>
   );
 };
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,25 @@
 import { combineReducers } from 'redux';
 import { rootReducer as transitReducer } from '@databraid/transit-widget/lib';
+import {
+  TRANSIT_WIDGET_ID,
+  SLACK_WIDGET_ID,
+  GITHUB_WIDGET_ID,
+} from '../constants';
 
-console.log(transitReducer);
-export default combineReducers({
-  transit_widget: transitReducer,
+const initialState = {
+  ids: [TRANSIT_WIDGET_ID],
+  byId: {},
+};
+
+const widgets = (state = initialState, action) => ({
+  ...state,
+  byId: {
+    [TRANSIT_WIDGET_ID]: transitReducer(state.byId[TRANSIT_WIDGET_ID], action),
+  },
 });
+
+const rootReducer = combineReducers({
+  widgets,
+});
+
+export default rootReducer;
