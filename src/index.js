41bdeb5b5789bc1826d6091156f0e2cfaca0d82f
Dave <wm.david.gallup@gmail.com>
2017-09-08 18:03:07 -0700

    Remove unnecessary css file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
-import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 import store from './store';
