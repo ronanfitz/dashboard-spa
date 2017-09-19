bdaa72aa79b3795c4faaf85ebcab97f7156091f5
Dave <wm.david.gallup@gmail.com>
2017-09-19 14:48:04 -0700

    Clean out auto-positioning functionality.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -48,72 +48,6 @@ export const collapseWidgetSidebars = (metadata) => {
 };
 
 
-const isYOverlap = (newY, newW, widgetY, widgetW) => {
-  const y1 = (
-    widgetY >= newY &&
-    widgetY <= (newY + newW) - 1
-  );
-  const y2 = (
-    (widgetY + widgetW) - 1 >= newY &&
-    (widgetY + widgetW) - 1 <= (newY + newW) - 1
-  );
-  const y3 = (
-    newY >= widgetY &&
-    newY <= (widgetY + widgetW) - 1
-  );
-  const y4 = (
-    (newY + newW) - 1 >= widgetY &&
-    (newY + newW) - 1 <= (widgetY + widgetW) - 1
-  );
-
-  return y1 || y2 || y3 || y4;
-};
-
-export const isValidLocation = (layout, x, y, w, h) => {
-  for (let i = 0; i < layout.length; i += 1) {
-    const x1 = (
-      layout[i].x >= x &&
-      layout[i].x <= (x + h) - 1 &&
-      isYOverlap(y, w, layout[i].y, layout[i].w)
-    );
-    const x2 = (
-      (layout[i].x + layout[i].h) - 1 >= x &&
-      (layout[i].x + layout[i].h) - 1 <= (x + h) - 1 &&
-      isYOverlap(y, w, layout[i].y, layout[i].w)
-    );
-    const x3 = (
-      x >= layout[i].x &&
-      x <= (layout[i].x + layout[i].h) - 1 &&
-      isYOverlap(y, w, layout[i].y, layout[i].w)
-    );
-    const x4 = (
-      (x + h) - 1 >= layout[i].x &&
-      (x + h) - 1 <= (layout[i].x + layout[i].h) - 1 &&
-      isYOverlap(y, w, layout[i].y, layout[i].w)
-    );
-
-    if (x1 || x2 || x3 || x4) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-export const calculateInitialPosition = (layout, width, height, maxCols = 12) => {
-  if (width > maxCols) {
-    return null;
-  }
-  for (let x = 0; x < 1000; x += 1) { // hardcoded upper limit to avoid unlikely infinite loop
-    for (let y = 0; y < maxCols - width; y += 1) {
-      if (isValidLocation(layout, x, y, width, height, maxCols)) {
-        return { x, y };
-      }
-    }
-  }
-  return null;
-};
-
 export const getWidgetConfigByType = (type) => {
   if (!type) {
     return undefined;
@@ -130,13 +64,9 @@ export const getWidgetConfigByType = (type) => {
 
 
 export const widgets = (state = initialState, action) => {
-  console.log('**LAYOUT BEFORE**\n', state.grid.layout);
-  console.log('**LAYOUT AFTER**\n',action.layout);
   switch (action.type) {
     case ADD_WIDGET: {
-      const newLoc = calculateInitialPosition(state.grid.layout, 6, 8);
       const widgetConfig = getWidgetConfigByType(action.id);
-      console.log('New Loc: ', newLoc);
       return {
         ...state,
         ids: [...state.ids, widgetConfig.type],
@@ -147,8 +77,8 @@ export const widgets = (state = initialState, action) => {
             ...state.grid.layout,
             {
               i: widgetConfig.type,
-              x: newLoc.x,
-              y: newLoc.y,
+              x: 0,
+              y: 0,
               w: widgetConfig.initWidth,
               h: widgetConfig.initHeight,
               minW: widgetConfig.minWidth,
